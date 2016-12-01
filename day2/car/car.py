b4c6f70e05e96064cdf29167c4805ecc5e19055c
bmuthoga <bmuthoga@gmail.com>
2016-12-01 20:16:52 +0300

    Update car.py

--- day2/car/car.py
+++ day2/car/car.py
@@ -1,20 +1,22 @@
 class Car(object):
-  def __init__(self, car_type = None , car_model = None, car_name =None ):
+  def __init__(self, car_type = None , model = None, name =None ):
     self.car_type = car_type
     self.speed = 0
-    if car_model is None:
-      self.car_model = "GM"
+    self.num_of_doors = 2
+    if model is None:
+      self.model = "GM"
     else:
-      self.car_model = car_model
-    if car_name is None:
-      self.car_name = "General"
+      self.model = model
+    if name is None:
+      self.name = "General"
     else:
-      self.car_name = car_name
+      self.name = name
 
   def car_doors(self):
    if self.car_type != "porshe" or "Koenigsegg":
-     return 4
-   return 2
+     self.num_of_doors = 4
+     return self
+   return self.num_of_doors
 
   def num_of_wheels(self):
     if self.car_type != "trailer":
@@ -23,24 +25,24 @@ class Car(object):
       return 8
 
   def drive(self, number):
-      if self.car_type == "trailer":
-          if number == 7:
-              self.speed = 77
-              return self
-          if number <= 1 and number <= 7:
-              self.speed = 30
-              return self
-          if number > 7:
-              self.speed = 0
-              return self
-          else:
-              if number >= 1:
-              self.speed = 1000
-              return self
+    if self.car_type == "trailer":
+      if number == 7:
+        self.speed = 77
+        return self
+      if number <= 1 and number <= 7:
+        self.speed = 30
+        return self
+      if number > 7:
+        self.speed = 0
+        return self
       else:
+        if number >= 1:
+          self.speed = 1000
           return self
+    else:
+      return self
 
-  def is_saloon(self, number):
-      if self.car_type == "saloon":
-          return True
-      return False
+  def is_saloon(self, number, car_type):
+    if self.car_type == "saloon":
+      return True
+    return False
