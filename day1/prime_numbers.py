98ba4be92e6f2e764808317f2ae3d549f728e72d
bmuthoga <bmuthoga@gmail.com>
2016-12-01 17:10:34 +0300

    Create prime_numbers.py

---
+++ day1/prime_numbers.py
@@ -0,0 +1,16 @@
+def prime_numbers(number):
+	list=[]
+	if number < 0:
+		return "Only positive numbers allowed."
+	if number < 2:
+		return "No prime number existing for given input."
+	if type(number) != int:
+		return "Only integers allowed."
+	for i in range(2,number+1):
+		if i>1:
+			for x in range(2,i):
+				if(i%x)==0:
+					break
+			else:
+				list.append(i)
+	return list
